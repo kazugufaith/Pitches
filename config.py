04c26beacdc2b010b6aa3bfd3a93e90d698134c2
kazugufaith <faithmugesia@gmail.com>
2019-05-28 11:35:44 +0300

    added config.py file

--- config.py
+++ config.py
@@ -1,51 +1,51 @@
-# import os
+import os
 
-# class Config:
-#     '''
-#     General configuration parent class
-#     '''
-#     SECRET_KEY = os.environ.get('SECRET_KEY')
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1234@localhost/pitch'
-#     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1234@localhost/pitch'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
-#     # email configurations
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-#     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
-# class TestConfig(Config):
-#     '''
-#     Testing configuration child class
+class TestConfig(Config):
+    '''
+    Testing configuration child class
 
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2:/moringa:1234@localhost/pitch'
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2:/moringa:1234@localhost/pitch'
 
-# class ProdConfig(Config):
-#     '''
-#     Production  configuration child class
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
 
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
-# class DevConfig(Config):
-#     '''
-#     Development  configuration child class
+class DevConfig(Config):
+    '''
+    Development  configuration child class
 
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#     ENV = 'development'
-#     DEBUG = True
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    ENV = 'development'
+    DEBUG = True
 
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig,
-# 'test':TestConfig
-# }
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
