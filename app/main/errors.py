6e316430f5a45aebfcd8a60d4e6b3cf2b951af2a
kazugufaith <faithmugesia@gmail.com>
2019-05-28 10:17:04 +0300

    create function to render the 404 error page

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,9 +1,10 @@
-# from flask import render_template
-# from . import main
+from flask import render_template
+from . import main
 
-# @main.app_errorhandler(404)
-# def error_404(error):
-#     """
-#     Function to render the 404 error page
-#     """
-#     return render_template('404.html'), 404
+@main.app_errorhandler(404)
+def error_404(error):
+    """
+    Function to render the 404 error page
+    """
+    return render_template('404.html'), 404
+    
