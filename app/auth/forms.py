04a9a14f40c89f30e6c6a54a3062b7a760e828dd
kazugufaith <faithmugesia@gmail.com>
2019-05-28 10:07:03 +0300

    added a validation error where when user enters an exist email he gets a message and choose another name

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -13,9 +13,9 @@ class RegistrationForm(FlaskForm):
     password_confirm = PasswordField('Confirm password', validators=[Required(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Sign Up')
 
-#     def validate_email(self, data_field):
-#         if User.query.filter_by(email=data_field.data).first():
-#             raise ValidationError('There is an account with that email')
+    def validate_email(self, data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError('There is an account with that email')
 
 # class LoginForm(FlaskForm):
 #     email = StringField('Your Email Address', validators=[Required(), Email()])
