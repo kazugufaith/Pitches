193c0e95cee1a4c254ef335eaaf965accf4c9f7b
kazugufaith <faithmugesia@gmail.com>
2019-05-28 10:02:42 +0300

    added init.py file

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,5 @@
-# from flask import Blueprint
+from flask import Blueprint
 
-# auth = Blueprint('auth', __name__)
+auth = Blueprint('auth', __name__)
 
-# from . import views, formsgit
+from . import views, forms
