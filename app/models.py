baf64acd9955ff57a5ed6a2ca22e71a95a2bbd1c
kazugufaith <faithmugesia@gmail.com>
2019-05-28 11:12:18 +0300

    added function that retrieves a user on models.py file

--- app/models.py
+++ app/models.py
@@ -1,14 +1,14 @@
-# from . import db, login_manager
-# from werkzeug.security import generate_password_hash, check_password_hash
-# from flask_login import UserMixin
-# from datetime import datetime
+from . import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     """
-#     Function that retrieves a user
-#     """
-#     return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    """
+    Function that retrieves a user
+    """
+    return User.query.get(int(user_id))
 
 # class User(UserMixin, db.Model):
 #     """
