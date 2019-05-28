05b82f018bcff4611292bf784e55bdeda446098b
kazugufaith <faithmugesia@gmail.com>
2019-05-28 11:05:26 +0300

    added registration to the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -31,11 +31,11 @@ def create_app(config_name):
     login_manager.init_app(app)
     mail.init_app(app)
 
-    # # Registering the blueprint
-    # from .main import main as main_blueprint
-    # app.register_blueprint(main_blueprint)
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-    # from .auth import auth as auth_blueprint
-    # app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
-    # return app
+    return app
